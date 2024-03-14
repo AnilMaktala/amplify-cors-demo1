a0998e7f11a654262585a6d54e388f3232641fd3
Anil Maktala <amaktala@amazon.com>
2024-03-14 10:47:30 -0400

    initial

---
+++ react-amplified/src/App.jsx
@@ -0,0 +1,82 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import { generateClient } from "aws-amplify/api";
+import { listBlogs } from "./graphql/queries";
+import { subscribeToLiveMessages } from "./graphql/subscriptions";
+import { broadcastLiveMessage } from "./graphql/mutations";
+
+const client = generateClient();
+
+function App() {
+  const [blogs, setBlogs] = useState([]);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // fetches all blog posts
+    async function fetchBlogs() {
+      const response = await client.graphql({
+        query: listBlogs,
+      });
+      setBlogs(response.data.listBlogs.items);
+    }
+    fetchBlogs();
+
+    // setup subscriptions for live chat messages
+    const subscription = client
+      .graphql({
+        query: subscribeToLiveMessages,
+      })
+      .subscribe((next) => {
+        setMessages((messages) => [
+          ...messages,
+          next.data.subscribeToLiveMessages,
+        ]);
+      });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  // sends the live chat message to users
+  function handleMessageSend(event) {
+    if (event.key === "Enter") {
+      client.graphql({
+        query: broadcastLiveMessage,
+        variables: {
+          message: event.target.value,
+        },
+      });
+    }
+  }
+
+  return (
+    <div style={{ display: "flex", gap: 20 }}>
+      <div>
+        <h1>Articles</h1>
+        {blogs.map((blog) => (
+          <div
+            style={{ border: "1px solid black", padding: 10, borderRadius: 10 }}
+          >
+            <h2>{blog.title}</h2>
+            <p>{blog.content}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h1>Live chat</h1>
+        <input
+          type="text"
+          placeholder="Hit enter to send message"
+          onKeyDown={handleMessageSend}
+        />
+        <hr></hr>
+        <ul>
+          {messages.map((message) => (
+            <li>{message}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-amplified/src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure({
+  API: {
+    GraphQL: {
+      endpoint: "https://api-east.amaktala.com/graphql",
+      defaultAuthMode: "apiKey",
+      apiKey: "da2-wla2u3mdrvcc7fv4uzs4rw2grm",
+      region: "us-east-1",
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
